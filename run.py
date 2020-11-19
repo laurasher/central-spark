0b45b6b2663beec5eb030299ef7ff04a77537a35
Laura Asher <laura.asher@jhuapl.edu>
2020-11-19 13:03:47 -0500

    change name to index

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-	return render_template("mockup.html")
+	return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
